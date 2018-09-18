1db93639fe53e448465c6f2c071c5f6d3d5290a7
Manish Magnani <parthpatel454500@gmail.com>
2018-09-18 16:24:59 +0530

    error handle in fb-callback

--- fb-callback.php
+++ fb-callback.php
@@ -18,17 +18,20 @@
         header('Location:login.php');
         exit();
     }
-
-    $OAuth2Client = $FB->getOAuth2Client();
-    if(!$accessToken->isLongLived())
-    {
-        $accessToken =$OAuth2Client->getLongLivedAceessToken($accessToken);
+    try{
+        $OAuth2Client = $FB->getOAuth2Client();
+        if(!$accessToken->isLongLived())
+        {
+            $accessToken =$OAuth2Client->getLongLivedAceessToken($accessToken);
+        }
+        $response = $FB->get("/me?fields=id,first_name,last_name,email",$accessToken);
+        $userdata =$response->getGraphNode()->asArray();
+        $_SESSION['userData']=$userdata;
+        $_SESSION['access_token']=(string)$accessToken;
+        header('location:index.php');
+    } catch(Facebook\Exceptions\FacebookResponseException $e){
+        header('location:error.html');
     }
-    $response = $FB->get("/me?fields=id,first_name,last_name,email",$accessToken);
-    $userdata =$response->getGraphNode()->asArray();
-    $_SESSION['userData']=$userdata;
-    $_SESSION['access_token']=(string)$accessToken;
-    header('location:index.php');
 
     // $_SESSION['access_token'] = (string)$accessToken;
     // require_once 'login.php';
